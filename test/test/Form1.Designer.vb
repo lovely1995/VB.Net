b83d0728e7bb0f7d101ec96d956ae2fe088c70dc
lovely1995 <35024069+lovely1995@users.noreply.github.com>
2020-09-20 15:40:00 +0800

    VB_test

---
+++ test/test/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.del = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.display = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.update_btn = New System.Windows.Forms.Button()
+        Me.add_btn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.DarkOrchid
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("微軟正黑體", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button5.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button5.Location = New System.Drawing.Point(144, 448)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(99, 37)
+        Me.Button5.TabIndex = 58
+        Me.Button5.Text = "search"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("微軟正黑體", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label4.Location = New System.Drawing.Point(32, 21)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(293, 42)
+        Me.Label4.TabIndex = 66
+        Me.Label4.Text = "Database System"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(474, 85)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(604, 437)
+        Me.DataGridView1.TabIndex = 95
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(191, 219)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(191, 25)
+        Me.TextBox3.TabIndex = 93
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'del
+        '
+        Me.del.BackColor = System.Drawing.Color.Tomato
+        Me.del.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.del.Font = New System.Drawing.Font("微軟正黑體", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.del.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.del.Location = New System.Drawing.Point(249, 297)
+        Me.del.Name = "del"
+        Me.del.Size = New System.Drawing.Size(99, 37)
+        Me.del.TabIndex = 86
+        Me.del.Text = "del"
+        Me.del.UseVisualStyleBackColor = False
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(1031, 545)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(59, 25)
+        Me.TextBox4.TabIndex = 94
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'display
+        '
+        Me.display.BackColor = System.Drawing.Color.SlateBlue
+        Me.display.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.display.Font = New System.Drawing.Font("微軟正黑體", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.display.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.display.Location = New System.Drawing.Point(474, 536)
+        Me.display.Name = "display"
+        Me.display.Size = New System.Drawing.Size(99, 37)
+        Me.display.TabIndex = 87
+        Me.display.Text = "display"
+        Me.display.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("微軟正黑體", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label5.Location = New System.Drawing.Point(956, 548)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 22)
+        Me.Label5.TabIndex = 89
+        Me.Label5.Text = "ID"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(191, 159)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(191, 25)
+        Me.TextBox2.TabIndex = 92
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'update_btn
+        '
+        Me.update_btn.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.update_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.update_btn.Font = New System.Drawing.Font("微軟正黑體", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.update_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.update_btn.Location = New System.Drawing.Point(144, 297)
+        Me.update_btn.Name = "update_btn"
+        Me.update_btn.Size = New System.Drawing.Size(99, 37)
+        Me.update_btn.TabIndex = 88
+        Me.update_btn.Text = "update"
+        Me.update_btn.UseVisualStyleBackColor = False
+        '
+        'add_btn
+        '
+        Me.add_btn.BackColor = System.Drawing.Color.LimeGreen
+        Me.add_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.add_btn.Font = New System.Drawing.Font("微軟正黑體", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.add_btn.ForeColor = System.Drawing.Color.White
+        Me.add_btn.Location = New System.Drawing.Point(39, 297)
+        Me.add_btn.Name = "add_btn"
+        Me.add_btn.Size = New System.Drawing.Size(99, 37)
+        Me.add_btn.TabIndex = 90
+        Me.add_btn.Text = "add"
+        Me.add_btn.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(191, 96)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(191, 25)
+        Me.TextBox1.TabIndex = 91
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(39, 417)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(309, 25)
+        Me.TextBox5.TabIndex = 99
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 106)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 15)
+        Me.Label1.TabIndex = 101
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(36, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 15)
+        Me.Label2.TabIndex = 102
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(36, 229)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 15)
+        Me.Label3.TabIndex = 103
+        Me.Label3.Text = "Label3"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(172, 388)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 15)
+        Me.Label6.TabIndex = 104
+        Me.Label6.Text = "Label6"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1102, 585)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.del)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.display)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.update_btn)
+        Me.Controls.Add(Me.add_btn)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label4)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "DB SYSTEM"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents del As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents display As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents update_btn As Button
+    Friend WithEvents add_btn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ test/test/Form1.vb
@@ -0,0 +1,162 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim datanum As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\YuYu\Desktop\app\test\test\Database1.mdf;Integrated Security=True"
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+        show_data()
+    End Sub
+
+
+
+    Public Sub show_data()
+        Try
+            cmd = con.CreateCommand
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "select * from infotable"
+            cmd.ExecuteNonQuery()
+            Dim dt As New DataTable()
+            Dim da As New SqlDataAdapter(cmd)
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+        Catch ex As Exception
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            TextBox4.Text = ""
+
+        End Try
+    End Sub
+
+
+    Private Sub update_btn_Click(sender As Object, e As EventArgs) Handles update_btn.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        '使用前端欄位變數 需要雙括號
+        cmd.CommandText = "
+        update infotable set name='" & TextBox1.Text & "',uid='" & TextBox2.Text & "',city='" & TextBox3.Text & "' where Id='" & TextBox4.Text & "'
+        "
+        cmd.ExecuteNonQuery()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        show_data()
+    End Sub
+
+    Private Sub display_Click_1(sender As Object, e As EventArgs) Handles display.Click
+        show_data()
+    End Sub
+
+    Private Sub del_Click(sender As Object, e As EventArgs) Handles del.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        '使用前端欄位變數 需要雙括號
+        cmd.CommandText = "delete from infotable where Id ='" & TextBox4.Text & "'"
+        cmd.ExecuteNonQuery()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+
+        show_data()
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+
+        Try
+
+            datanum = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+            'MessageBox.Show(datanum)
+
+            '連接
+            cmd = con.CreateCommand()
+            '語法text
+            cmd.CommandType = CommandType.Text
+            'sql
+            cmd.CommandText = "select * from infotable where id =" & datanum
+            '執行
+            cmd.ExecuteNonQuery()
+
+            '??????
+            Dim dt As New DataTable()
+            Dim da As New SqlDataAdapter(cmd)
+            da.Fill(dt)
+
+            'dr = sql資料群
+            Dim dr As SqlClient.SqlDataReader
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            While dr.Read
+                TextBox1.Text = dr.GetString(1).ToString()
+                TextBox2.Text = dr.GetString(2).ToString()
+                TextBox3.Text = dr.GetString(3).ToString()
+                TextBox4.Text = dr.GetInt32(0)
+            End While
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub add_btn_Click(sender As Object, e As EventArgs) Handles add_btn.Click
+        Try
+            cmd = con.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "insert into infotable  ([name],[uid],[city]) VALUES ('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "')"
+            cmd.ExecuteNonQuery()
+
+            show_data()
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            TextBox4.Text = ""
+            MessageBox.Show("done")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+        con.Open()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "
+        select * from infotable where name like '%" & TextBox5.Text & "'
+        union 
+        select * from infotable where name like '" & TextBox5.Text & "%'
+        union 
+        select * from infotable where city like '%" & TextBox5.Text & "'
+        union 
+        select * from infotable where city like '" & TextBox5.Text & "%'
+        "
+
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+End Class
